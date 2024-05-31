3ba82d2bba5b9e9597f3030ffc44769180b2a3a3
Mari Beglaryan <mary.beglaryan@bk.ru>
2024-05-31 20:41:39 +0400

    done some work

--- Task 2.2/Program.cs
+++ Task 2.2/Program.cs
@@ -4,10 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello. Please enter any message.");
-            var userInput = Console.ReadLine();
-            Console.WriteLine($"Your name is {userInput}.");
-            Console.Clear();
+            
         }
     }
 }
